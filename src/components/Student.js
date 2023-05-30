06d0ad097d5a1cbc40d697fbfedb4aa76cd23a93
Rifatpomil <130542993+Rifatpomil@users.noreply.github.com>
2023-05-30 10:21:22 +0600

    Add files via upload

---
+++ src/components/Student.js
@@ -0,0 +1,81 @@
+import React, { useEffect, useState } from 'react';
+import {makeStyles} from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+import { Container,Paper,Button } from '@material-ui/core/';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+      '& > *': {
+        margin: theme.spacing(1),
+       
+      },
+    },
+  }));
+export default function Student() {
+    const paperStyle={padding:'50px 20px', width:600,margin:"20px auto"}
+    const[name,setName]=useState('')
+    const[address,setAddress]=useState('')
+    const[ID,setID]=useState('')
+    const[students,setStudents]=useState([])
+    const classes = useStyles()
+
+    const handleClick=(e)=>{
+        e.preventDefault()
+        const student={name,address,ID}
+        console.log(student)
+        fetch("http://localhost:8080/student/add",{
+         method:"Post",
+         headers:{"Content-Type":"application/json"},
+         body:JSON.stringify(student)
+        
+    }).then(()=>{
+      console.log("New Student Has Been Added")
+    })
+  }
+useEffect(()=>{
+  fetch("http://localhost:8080/student/getAll")
+  .then(res=>res.json())
+  .then((result)=>{
+    setStudents(result);
+  }
+)
+},[])
+  return (
+    <Container>
+        <Paper elevation={3} style={paperStyle}>
+            <h1 style={{color:"blue"}}><u>Add Student</u></h1>
+    <form className={classes.root} noValidate autoComplete="off">
+      <TextField id="outlined-basic" label="Student Name" variant="outlined" fullwidth 
+      value={name}
+      onChange={(e)=>setName(e.target.value)}
+      />
+      <TextField id="outlined-basic" label="Student Address" variant="outlined" fullwidth
+      value={address}
+      onChange={(e)=>setAddress(e.target.value)}
+      />
+      <TextField id="outlined-basic" label="Student ID" variant="outlined" fullwidth
+      value={ID}
+      onChange={(e)=>setID(e.target.value)}
+      />
+      <Button variant="contained" color="secondary" onClick={handleClick}>
+       Submit
+</Button>
+    </form>
+    </Paper> 
+    <h1>Students</h1>
+   
+    <Paper elevation={3} style={paperStyle}>
+      
+      {students.map(student=>(
+        <Paper elevation={6} style={{margin:"10px", padding:"15pm",textAlign:"left"}}key={student.id} >
+          Student serial number is:{student.id}<br/>
+          Student Name is:{student.name}<br/>
+          Student Address is:{student.address}<br/>
+          </Paper>
+      ))
+      }
+
+    </Paper>
+    </Container>
+  );
+}
